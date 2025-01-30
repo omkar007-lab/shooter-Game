9610bb43e53ccba7538ad723f6c0c0f241cc148c
omkar shewale <omkarshewale205@gmail.com>
2025-01-30 20:16:15 +0530

    Update game.py

--- game.py
+++ game.py
@@ -15,11 +15,9 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
-#set framerate
 clock = pygame.time.Clock()
 FPS = 60
 
-#define game variables
 GRAVITY = 0.75
 SCROLL_THRESH = 200
 ROWS = 16
@@ -33,19 +31,12 @@ level = 1
 start_game = False
 start_intro = False
 
-
-#define player action variables
 moving_left = False
 moving_right = False
 shoot = False
 grenade = False
 grenade_thrown = False
 
-
-#load music and sounds
-#pygame.mixer.music.load('audio/music2.mp3')
-#pygame.mixer.music.set_volume(0.3)
-#pygame.mixer.music.play(-1, 0.0, 5000)
 jump_fx = pygame.mixer.Sound('audio/jump.wav')
 jump_fx.set_volume(0.05)
 shot_fx = pygame.mixer.Sound('audio/shot.wav')
@@ -53,39 +44,33 @@ shot_fx.set_volume(0.05)
 grenade_fx = pygame.mixer.Sound('audio/grenade.wav')
 grenade_fx.set_volume(0.05)
 
-
-#load images
-#button images
 start_img = pygame.image.load('img/start_btn.png').convert_alpha()
 exit_img = pygame.image.load('img/exit_btn.png').convert_alpha()
 restart_img = pygame.image.load('img/restart_btn.png').convert_alpha()
-#background
+
 pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
 pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
 mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
 sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
-#store tiles in a list
+
 img_list = []
 for x in range(TILE_TYPES):
-	img = pygame.image.load(f'img/Tile/{x}.png')
-	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
-	img_list.append(img)
-#bullet
+    img = pygame.image.load(f'img/Tile/{x}.png')
+    img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+    img_list.append(img)
+
 bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
-#grenade
 grenade_img = pygame.image.load('img/icons/grenade.png').convert_alpha()
-#pick up boxes
+
 health_box_img = pygame.image.load('img/icons/health_box.png').convert_alpha()
 ammo_box_img = pygame.image.load('img/icons/ammo_box.png').convert_alpha()
 grenade_box_img = pygame.image.load('img/icons/grenade_box.png').convert_alpha()
 item_boxes = {
-	'Health'	: health_box_img,
-	'Ammo'		: ammo_box_img,
-	'Grenade'	: grenade_box_img
+    'Health'    : health_box_img,
+    'Ammo'      : ammo_box_img,
+    'Grenade'   : grenade_box_img
 }
 
-
-#define colours
 BG = (144, 201, 120)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
@@ -93,13 +78,11 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 PINK = (235, 65, 54)
 
-#define font
 font = pygame.font.SysFont('Futura', 30)
 
 def draw_text(text, font, text_col, x, y):
-	img = font.render(text, True, text_col)
-	screen.blit(img, (x, y))
-
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 def draw_bg():
 	screen.fill(BG)
@@ -111,7 +94,7 @@ def draw_bg():
 		screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
 
 
-#function to reset level
+
 def reset_level():
 	enemy_group.empty()
 	bullet_group.empty()
@@ -122,7 +105,7 @@ def reset_level():
 	water_group.empty()
 	exit_group.empty()
 
-	#create empty tile list
+	
 	data = []
 	for row in range(ROWS):
 		r = [-1] * COLS
@@ -154,105 +137,84 @@ class Soldier(pygame.sprite.Sprite):
 		self.frame_index = 0
 		self.action = 0
 		self.update_time = pygame.time.get_ticks()
-		#ai specific variables
 		self.move_counter = 0
-		self.vision = pygame.Rect(0, 0, 150, 20)
-		self.idling = False
-		self.idling_counter = 0
-		
-		#load all images for the players
-		animation_types = ['Idle', 'Run', 'Jump', 'Death']
-		for animation in animation_types:
-			#reset temporary list of images
-			temp_list = []
-			#count number of files in the folder
-			num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
-			for i in range(num_of_frames):
-				img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
-				img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-				temp_list.append(img)
-			self.animation_list.append(temp_list)
+self.vision = pygame.Rect(0, 0, 150, 20)
+self.idling = False
+self.idling_counter = 0
 
-		self.image = self.animation_list[self.action][self.frame_index]
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
-		self.width = self.image.get_width()
-		self.height = self.image.get_height()
+animation_types = ['Idle', 'Run', 'Jump', 'Death']
+for animation in animation_types:
+    temp_list = []
+    num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+    for i in range(num_of_frames):
+        img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
+        img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        temp_list.append(img)
+    self.animation_list.append(temp_list)
 
+self.image = self.animation_list[self.action][self.frame_index]
+self.rect = self.image.get_rect()
+self.rect.center = (x, y)
+self.width = self.image.get_width()
+self.height = self.image.get_height()
 
-	def update(self):
-		self.update_animation()
-		self.check_alive()
-		#update cooldown
-		if self.shoot_cooldown > 0:
-			self.shoot_cooldown -= 1
+def update(self):
+    self.update_animation()
+    self.check_alive()
+    if self.shoot_cooldown > 0:
+        self.shoot_cooldown -= 1
 
+def move(self, moving_left, moving_right):
+    screen_scroll = 0
+    dx = 0
+    dy = 0
 
-	def move(self, moving_left, moving_right):
-		#reset movement variables
-		screen_scroll = 0
-		dx = 0
-		dy = 0
+    if moving_left:
+        dx = -self.speed
+        self.flip = True
+        self.direction = -1
+    if moving_right:
+        dx = self.speed
+        self.flip = False
+        self.direction = 1
 
-		#assign movement variables if moving left or right
-		if moving_left:
-			dx = -self.speed
-			self.flip = True
-			self.direction = -1
-		if moving_right:
-			dx = self.speed
-			self.flip = False
-			self.direction = 1
+    if self.jump == True and self.in_air == False:
+        self.vel_y = -11
+        self.jump = False
+        self.in_air = True
 
-		#jump
-		if self.jump == True and self.in_air == False:
-			self.vel_y = -11
-			self.jump = False
-			self.in_air = True
+    self.vel_y += GRAVITY
+    if self.vel_y > 10:
+        self.vel_y
+    dy += self.vel_y
 
-		#apply gravity
-		self.vel_y += GRAVITY
-		if self.vel_y > 10:
-			self.vel_y
-		dy += self.vel_y
+    for tile in world.obstacle_list:
+        if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+            dx = 0
+            if self.char_type == 'enemy':
+                self.direction *= -1
+                self.move_counter = 0
+        if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+            if self.vel_y < 0:
+                self.vel_y = 0
+                dy = tile[1].bottom - self.rect.top
+            elif self.vel_y >= 0:
+                self.vel_y = 0
+                self.in_air = False
+                dy = tile[1].top - self.rect.bottom
 
-		#check for collision
-		for tile in world.obstacle_list:
-			#check collision in the x direction
-			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-				dx = 0
-				#if the ai has hit a wall then make it turn around
-				if self.char_type == 'enemy':
-					self.direction *= -1
-					self.move_counter = 0
-			#check for collision in the y direction
-			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-				#check if below the ground, i.e. jumping
-				if self.vel_y < 0:
-					self.vel_y = 0
-					dy = tile[1].bottom - self.rect.top
-				#check if above the ground, i.e. falling
-				elif self.vel_y >= 0:
-					self.vel_y = 0
-					self.in_air = False
-					dy = tile[1].top - self.rect.bottom
+    if pygame.sprite.spritecollide(self, water_group, False):
+        self.health = 0
 
+    level_complete = False
+    if pygame.sprite.spritecollide(self, exit_group, False):
+        level_complete = True
 
-		#check for collision with water
-		if pygame.sprite.spritecollide(self, water_group, False):
-			self.health = 0
+    if self.rect.bottom > SCREEN_HEIGHT:
+        self.health = 0
 
-		#check for collision with exit
-		level_complete = False
-		if pygame.sprite.spritecollide(self, exit_group, False):
-			level_complete = True
-
-		#check if fallen off the map
-		if self.rect.bottom > SCREEN_HEIGHT:
-			self.health = 0
-
-
-		#check if going off the edges of the screen
+		remove the all comments and give me all code agin
+#check if going off the edges of the screen
 		if self.char_type == 'player':
 			if self.rect.left + dx < 0 or self.rect.right + dx > SCREEN_WIDTH:
 				dx = 0
@@ -344,8 +306,6 @@ class Soldier(pygame.sprite.Sprite):
 			#update the animation settings
 			self.frame_index = 0
 			self.update_time = pygame.time.get_ticks()
-
-
 
 	def check_alive(self):
 		if self.health <= 0:
