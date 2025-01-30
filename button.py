2f2bcfa1e2a32e6444126d4de9ffc673650e5a71
omkar shewale <omkarshewale205@gmail.com>
2025-01-30 20:03:32 +0530

    Update button.py

--- button.py
+++ button.py
@@ -1,8 +1,7 @@
-import pygame 
+import pygame
 
-#button class
 class Button():
-	def __init__(self,x, y, image, scale):
+	def __init__(self, x, y, image, scale):
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -12,11 +11,8 @@ class Button():
 
 	def draw(self, surface):
 		action = False
-
-		#get mouse position
 		pos = pygame.mouse.get_pos()
 
-		#check mouseover and clicked conditions
 		if self.rect.collidepoint(pos):
 			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
 				action = True
@@ -25,7 +21,6 @@ class Button():
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
 
-		#draw button
 		surface.blit(self.image, (self.rect.x, self.rect.y))
 
 		return action
